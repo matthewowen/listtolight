cbb48a150d8104b311d449e27e8224e1920d2b98
Matthew Owen <matthew.owen95@gmail.com>
2012-07-15 16:09:28 +0100

    incorporate bugfix#

--- javascripts/ltl/ltl.js
+++ javascripts/ltl/ltl.js
@@ -44,23 +44,6 @@ function listToLight(selector) {
             } else {
                 $('.ltl-scroller.right').css('display', 'none');
             }
-            // bind the key events.
-            $(document).keydown(function (e) {
-                // We don't need to worry about unbinding these events
-                // This is because hideLightbox() doesn't do anything of
-                // significance if the lightbox is already hidden, and
-                // scrollGallery() only scrolls if there's something to
-                // scroll to.
-                if (e.keyCode === 27) {
-                    that.hideLightbox();
-                }
-                if (e.keyCode === 37) {
-                    that.scrollGallery('left');
-                }
-                if (e.keyCode === 39) {
-                    that.scrollGallery('right');
-                }
-            });
         },
 
         scrollGallery: function (dir) {
@@ -119,6 +102,22 @@ function listToLight(selector) {
             this.findImagesInLists().click(function () {
                 that.switchImage(this);
                 that.showLightbox();
+            });
+            $(document).keydown(function (e) {
+                // We don't need to worry about unbinding these events
+                // This is because hideLightbox() doesn't do anything of
+                // significance if the lightbox is already hidden, and
+                // scrollGallery() only scrolls if there's something to
+                // scroll to.
+                if (e.keyCode === 27) {
+                    that.hideLightbox();
+                }
+                if (e.keyCode === 37) {
+                    that.scrollGallery('left');
+                }
+                if (e.keyCode === 39) {
+                    that.scrollGallery('right');
+                }
             });
         }
     };
